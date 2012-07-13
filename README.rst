8ba8ac9aa85eba2bdb17df8dc6ec85f524854ffc
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2012-07-13 13:27:57 -0430

    First commit

---
+++ README.rst
@@ -0,0 +1,4 @@
+Introduction
+============
+
+This package is a Plone 4 policy product for misitio website, that create some content structure anspecial configuration for misitio website
---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='misitio.policy',
+      version=version,
+      description="A Plone 4 policy product for misitio website",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone policy product package misitio website',
+      author='Leonardo J. Caballero G.',
+      author_email='leonardocaballero@gmail.com',
+      url='https://github.com/plone-ve/misitio.policy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['misitio'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': ['plone.app.testing'],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
